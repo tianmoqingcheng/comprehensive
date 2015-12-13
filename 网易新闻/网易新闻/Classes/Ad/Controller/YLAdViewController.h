d088efea6c7d45673061efcbde249c326fb8f84a
天魔 <hnyangliuayyz@163.com>
2015-12-14 00:24:32 +0800

    项目初始化

---
+++ 网易新闻/网易新闻/Classes/Ad/Controller/YLAdViewController.h
@@ -0,0 +1,13 @@
+//
+//  YLAdViewController.h
+//  网易新闻
+//
+//  Created by mac on 15/12/13.
+//  Copyright © 2015年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YLAdViewController : UIViewController
+
+@end
---
+++ 网易新闻/网易新闻/Classes/Ad/Controller/YLAdViewController.m
@@ -0,0 +1,42 @@
+//
+//  YLAdViewController.m
+//  网易新闻
+//
+//  Created by mac on 15/12/13.
+//  Copyright © 2015年 mac. All rights reserved.
+//
+
+#import "YLAdViewController.h"
+
+@interface YLAdViewController ()
+
+@end
+
+@implementation YLAdViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
